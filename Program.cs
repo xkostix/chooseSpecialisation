6e205891aedbdae5230439b2554432bbce6b4806
xkostix <mkostik@yandex.ru>
2023-01-21 22:02:34 +0700

    Beginning of CSharp program

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
 ﻿//Программа из массива строк формирует массив из строк, длина которых не более 3 символов.
 
-string[] originArray = new string[5]
+void getArrayOfThreeChar();
+
+string[] originArray = new string["1234", "yes", "nono", "22", "54321"];
+
